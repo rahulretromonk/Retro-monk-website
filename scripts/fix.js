8966f3d1f1ebe6e987904b1b72559d000a70815f
Vijayendiran <vijayendiranmala@gmail.com>
2026-08-26 06:08:01 +0530

    Remove duplicate root-level project

--- scripts/fix.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let data = fs.readFileSync('src/lib/db.json', 'utf8');
-data = data.replace(/"eventDetails"/g, '"details"');
-fs.writeFileSync('src/lib/db.json', data);
-console.log('Done');
--- src/app/admin/faq/types.ts
+++
@@ -1,9 +0,0 @@
-export interface FAQItem {
-  id: string;
-  question: string;
-  answer: string;
-  displayOrder: number;
-  createdAt: string;
-}
-
-export type NewFAQItem = Omit<FAQItem, 'id' | 'createdAt'>;
